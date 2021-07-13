ee6a56805e1bf2570fc90cf968f98ae7c34464ed
Rudzky <lubieprogramowanie@gmail.com>
2021-07-13 13:07:04 +0200

    clean-up for template puropse

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import { formData } from "./forms";
-
-const form = document.querySelector("form")!;
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const data = formData(form);
-  console.log(data);
-});
+console.log("Hello, Friend");
